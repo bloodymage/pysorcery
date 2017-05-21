9eada256899ce6a45d9cfb06be86daed268bd179
G Derber <gd.smlinux@gmail.com>
2017-05-21 18:50:40 -0400

    Begin work on searching archive files

--- pysorcery/lib/util/files/archive/tar.py
+++ pysorcery/lib/util/files/archive/tar.py
@@ -125,6 +125,20 @@ def testarchive(filename):
 # Return: ...
 #
 #-------------------------------------------------------------------
+def search(filename):
+    logger.debug("Begin Function")    
+    logger.debug('End Function')
+    return
+
+#-------------------------------------------------------------------
+#
+# Function 
+#
+# Input:  ...
+# Output: ...
+# Return: ...
+#
+#-------------------------------------------------------------------
 def verify_extract():
     logger.debug('Begin Function')
 
