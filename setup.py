617768c0694839b8b1c7590aacaf73395c2db823
G Derber <gd.smlinux@gmail.com>
2017-06-30 19:59:32 -0400

    Fix setup.py, setup.py works

--- setup.py
+++ setup.py
@@ -26,29 +26,37 @@
 #    along with Sorcery.  If not, see <http://www.gnu.org/licenses/>.
 #
 #-------------------------------------------------------------------------------
-from setuputils import setup, find_packages
+from setuptools import setup, find_packages
 
 from codecs import open
 from os import path
 
 here = path.abspath(path.dirname(__file__))
 
+# added so distributors can consistently specify a private module location
+private_module_path = here
+if private_module_path:
+    import sys
+    sys.path.insert(1, private_module_path)
+
+try:
+    from pysorcery import __version__
+except ImportError as msg:
+    __version__ = '0.0.1'
+    print(msg)
+    
 # Get the long description from the README file
 #with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 #    long_description = f.read()
 
 setup(
-    name='pySorcery',
-    version='0.0.1',
-    
+    name='pysorcery',
+    version = __version__, 
     description='Sorcery Package Management System',
     long_description='Sorcery Package Management System ...',
-    
     url='https://www.github.com/gderber/python-sorcery/',
-
     author='Geoff S Derber',
     author_email='gd.smlinux@gmail.com',
-    
     license='GPLv3+',    
 
     classifiers=[
@@ -83,11 +91,11 @@ setup(
         'Environment :: Console :: Curses'
     ],
 
-    keywords='blah'
+    keywords='blah',
     
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    packages=find_packages(exclude=["bin", "doc*"]),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
@@ -97,7 +105,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['distro'],
+    install_requires=['distro>=1.0.2'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
@@ -111,36 +119,22 @@ setup(
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    #package_data={
-    #    'sample': ['package_data.dat'],
-    #},
+    package_data={
+        '': ['*.1'],
+    },
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    #data_files=[('my_data', ['data/data_file'])],
+    data_files=[('/usr/share/man/man1', ['docs/man/man1/pyarchive.1'])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'pyalter = pysorcery.cli.alter:main',
-            'pycabal = pysorcery.cli.cabal:main',
-            'pycast = pysorcery.cli.cast:main',
-            'pycleanse = pysorcery.cli.cleanse:main',
-            'pydispel = pysorcery.cli.dispel:main',
-            'pyresurrect = pysorcery.cli.resurrect:main',
-            'pyscribbler = pysorcery.cli.scribbler:main',
-            'pyscribe = pysorcery.cli.scribe:main',            
-            'pysorcery=pysorcery.cli.sorcery:main',
-            'pysummon = pysorcery.cli.summon:main',
-            'pyupstream = pysorcery.cli.upstream:main',
-            'pyvcast = pysorcery.cli.vcast:main'
-        ]
-        'gui_scripts': [
-            'pyxsorcery = pysorcery.gui.xsorcery'
+            'pyarchive = pysorcery.cli.archive:main'
         ]
     }
 )
